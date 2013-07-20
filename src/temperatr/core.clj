2ed9466fcf1d1e768ffc035ad687f7548008fc54
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-20 13:31:47 -0700

    Make an HTTP request against Google Maps to get info about the zipcode

--- src/temperatr/core.clj
+++ src/temperatr/core.clj
@@ -8,4 +8,18 @@
 (defn zipcode-search
   [zipcode]
   (println
-    (format "searching for zipcode %d..." zipcode)))
+    (format "searching for zipcode %d..." zipcode))
+
+  ; get an HTTP client
+  (require '[clj-http.client :as client])
+
+  ; hit Google Maps API for the zipcode
+  (def response
+    (client/get
+      (format
+        "http://maps.googleapis.com/maps/api/geocode/json?address=%d&sensor=false"
+        zipcode)))
+
+  ; print the response JSON for now
+  (println response)
+)
