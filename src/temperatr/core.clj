56b584ce2791764c6ec7b1bd4119576d405dd9ca
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-20 14:26:58 -0700

    Move zipcode-search into its own namespace

--- src/temperatr/core.clj
+++ src/temperatr/core.clj
@@ -1,23 +1,7 @@
 (ns temperatr.core
-  (:require [clj-http.client :as client]))
-
-(defn zipcode-search
-  [zipcode]
-  (println
-    (format "searching for zipcode %d..." zipcode))
-
-  ; hit Google Maps API for the zipcode
-  (def response
-    (client/get
-      (format
-        "http://maps.googleapis.com/maps/api/geocode/json?address=%d&sensor=false"
-        zipcode)))
-
-  ; print the response JSON for now
-  (println response)
-)
+  (:require [temperatr.zipcode :as zipcode]))
 
 (defn -main
   "Start temperatr"
   []
-  (zipcode-search 93312))
+  (zipcode/zipcode-search 93312))
---
+++ src/temperatr/zipcode.clj
@@ -0,0 +1,18 @@
+(ns temperatr.zipcode
+  (:require [clj-http.client :as client]))
+
+(defn zipcode-search
+  [zipcode]
+  (println
+    (format "searching for zipcode %d..." zipcode))
+
+  ; hit Google Maps API for the zipcode
+  (def response
+    (client/get
+      (format
+        "http://maps.googleapis.com/maps/api/geocode/json?address=%d&sensor=false"
+        zipcode)))
+
+  ; print the response JSON for now
+  (println response)
+)
