cdbc071904dcf950ddece5598a60faaa0fa5eaf3
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-20 12:26:02 -0700

    Initialize project structure with leiningen

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject temperatr "0.1.0-SNAPSHOT"
+  :description "A silly project in Clojure to search for nearby locales with more favorable weather."
+  :url "https://github.com/kevinmershon/temperatr"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
