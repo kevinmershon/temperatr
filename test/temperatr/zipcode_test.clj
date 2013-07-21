86c168b2b07b014b9758b25ef4d99313b3e14153
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-21 12:27:42 -0700

    Add some simple tests for zipcode search

---
+++ test/temperatr/zipcode_test.clj
@@ -0,0 +1,11 @@
+(ns temperatr.zipcode-test
+  (:require [clojure.test :refer :all]
+            [temperatr.zipcode :refer :all]))
+
+(deftest zipcode-invalid-test
+  (testing "Test an invalid zipcode"
+    (false? (temperatr.zipcode/search 123456789))))
+
+(deftest zipcode-valid-test
+  (testing "Test a valid zipcode"
+    (true? (temperatr.zipcode/search 90210))))
