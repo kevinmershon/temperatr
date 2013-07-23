23f13d255a0354d1e21a6f775f170cbea028db37
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-23 09:57:34 -0700

    Add test for get-point-at-bearing with all 0s. Formatting

--- test/temperatr/viewport_test.clj
+++ test/temperatr/viewport_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [temperatr.viewport :refer :all]))
 
+(deftest get-point-at-bearing-allballs-test
+  (testing "Test get-point-at-bearing with allballs"
+    (let [destination (#'temperatr.viewport/get-point-at-bearing 0.0 0.0 0.0 0)]
+      (is (= 0.0 (:latitude destination)))
+      (is (= 0.0 (:longitude destination))))))
+
 (deftest get-bounds-allballs-test
   (testing "Test get-bounds with allballs"
     (let [bounds (temperatr.viewport/get-bounds 0.0 0.0 0)]
@@ -13,5 +19,4 @@
       (is (= (:latitude (:north-west bounds)) 0.0))
       (is (= (:longitude (:north-west bounds)) 0.0))
       (is (= (:latitude (:south-east bounds)) 0.0))
-      (is (= (:longitude (:south-east bounds)) 0.0))
-    )))
+      (is (= (:longitude (:south-east bounds)) 0.0)))))
