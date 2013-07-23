6b5972156c208a65563d320dbf0ab29816211807
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-23 12:22:40 -0700

    Move most of the guts of exists? into get-location

--- src/temperatr/zipcode.clj
+++ src/temperatr/zipcode.clj
@@ -2,10 +2,11 @@
   (:require [clj-http.client :as client])
   (:require [clojure.data.json :as json]))
 
-(defn exists?
+(defn- get-location
+  "Use Google Geocoding API to get the lat/long location of a zipcode"
   [zipcode]
-  (println
-    (format "searching for zipcode %d..." zipcode))
+
+  (println (format "searching for zipcode %d..." zipcode))
 
   ; hit Google Maps API for the zipcode
   (let [response
@@ -22,7 +23,13 @@
 
     ; parse the response into JSON
     ; TODO -- add error handling
-    (let [response-json (json/read-str (:body response))]
-      ; check the status
-      ; either "OK" or "ZERO_RESULTS"
-      (= (get response-json "status") "OK"))))
+    (json/read-str (:body response))))
+
+(defn exists?
+  "Use Google Geocoding API to check if a zipcode exists"
+  [zipcode]
+
+  (let [response-json (get-location zipcode)]
+    ; check the status
+    ; either "OK" or "ZERO_RESULTS"
+    (= (get response-json "status") "OK")))
