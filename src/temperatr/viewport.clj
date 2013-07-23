b6e8b5bc76b3af5ef431966db39ff8cd9e769544
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-23 09:15:16 -0700

    Add a comment

--- src/temperatr/viewport.clj
+++ src/temperatr/viewport.clj
@@ -1,6 +1,8 @@
 (ns temperatr.viewport)
 
 (defn get-bounds
+  "Get the bounding viewport (northwest lat/long, southeast lat/long) for the
+  specified point and maximum distance in miles"
   [latitude longitude max-distance-in-miles]
   (println
     (format "calculating bounds for within %d miles of [%.4f,%.4f]"
