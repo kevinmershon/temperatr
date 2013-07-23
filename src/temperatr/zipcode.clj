4fb6dc0b0f68259061ad74628da9bf45257438c9
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-23 12:28:01 -0700

    More formatting

--- src/temperatr/zipcode.clj
+++ src/temperatr/zipcode.clj
@@ -10,12 +10,12 @@
 
   ; hit Google Maps API for the zipcode
   (let [response
-    (try
-      (client/get (format "http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%d&sensor=false"
-                          zipcode))
-    (catch Exception e
-      (println (.getMessage e))
-      (throw (Exception. "request failed!"))))]
+        (try
+          (client/get (format "http://maps.googleapis.com/maps/api/geocode/json?components=postal_code:%d&sensor=false"
+                              zipcode))
+          (catch Exception e
+            (println (.getMessage e))
+            (throw (Exception. "request failed!"))))]
 
     ; check for HTTP 200 (success)
     (if (not= (:status response) 200)
