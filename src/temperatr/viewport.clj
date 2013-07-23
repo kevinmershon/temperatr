8ba15b438d0b15d6da812ed71097e065e9dd6ca6
Kevin Mershon <nwlinkvxd@gmail.com>
2013-07-23 09:26:19 -0700

    Start building a private function to get a point a given distance away along a bearing

--- src/temperatr/viewport.clj
+++ src/temperatr/viewport.clj
@@ -1,5 +1,18 @@
 (ns temperatr.viewport)
 
+(defn- get-point-at-bearing
+  "Get the lat/long point at the specified distance in miles, along the
+  specified bearing"
+  [latitude longitude bearing distance-in-miles]
+
+  ; in java:
+  ;
+  ;var lat2 = Math.asin( Math.sin(lat1)*Math.cos(d/R) +
+  ;           Math.cos(lat1)*Math.sin(d/R)*Math.cos(brng) );
+  ;var lon2 = lon1 + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat1),
+  ;                  Math.cos(d/R)-Math.sin(lat1)*Math.sin(lat2));
+)
+
 (defn get-bounds
   "Get the bounding viewport (northwest lat/long, southeast lat/long) for the
   specified point and maximum distance in miles"
